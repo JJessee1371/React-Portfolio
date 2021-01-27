8ac3d2481f741af8385d7dff7a7ff34cb543aabc
Jon Jessee <tjessee7624@gmail.com>
2021-01-27 11:41:39 -0700

    Restructured navbar to include only a sidenav option and included CSS animation for homepage header.

--- src/Components/AboutHeader/AboutHeader.jsx
+++ src/Components/AboutHeader/AboutHeader.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './AboutHeader.css';
 
 //Homepage unique header
 function AboutHeader() {
+    const [effect, setEffect] = useState('header-text animated');
+
+    useEffect(() => {
+        setEffect('header-text animated bounce');
+    },[]);
+
     return (
         <header className="header-main">
-            <p id="line1" className="header-text">Passionate full stack developer.</p>
-            <p id="line2" className="header-text">Dedicated forward thinker.</p>
-            <p id="line3" className="header-text">Enthusiastic collaborator.</p>
+            <p id="line1" className={effect}>Passionate full stack developer.</p>
+            <p id="line2" className={effect}>Dedicated forward thinker.</p>
+            <p id="line3" className={effect}>Enthusiastic collaborator.</p>
         </header>
     );
 };
--- src/Components/Navbar/GlobalNavbar.jsx
+++ src/Components/Navbar/GlobalNavbar.jsx
@@ -1,42 +1,93 @@
 import React from 'react';
-import { Navbar, NavItem, Icon } from 'react-materialize';
-import { Link } from 'react-router-dom';
+import { Navbar, Icon, SideNav, SideNavItem, Button } from 'react-materialize';
 import logo from '../../images/logo.png';
 import 'materialize-css/dist/css/materialize.min.css';
-
+import './GlobalNavbar.css';
 
 class GlobalNavbar extends React.Component {
     render() {
         return (
-            <Navbar
-                className="nav"
-                alignLinks="right"
-                brand={<Link to="/"><img src={logo} width="180" height="60" alt="'Jessee' logo" className="brand-logo"></img></Link>}
-                // brand={<a className="brand-logo" href="/"><img src={logo} width="180" height="60" alt="'Jessee' logo" className="brand-logo"></img></a>}
-                id="mobile-nav"
-                menuIcon={<Icon>menu</Icon>}
-                options={{
-                    draggable: true,
-                    edge: 'left',
-                    inDuration: 250,
-                    onCloseEnd: null,
-                    onCloseStart: null,
-                    onOpenEnd: null,
-                    onOpenStart: null,
-                    outDuration: 200,
-                    preventScrolling: true
-                }}
-                >
-                <NavItem>
-                    <Link to="/">Home</Link>
-                </NavItem>
-                <NavItem>
-                    <Link to="/portfolio">Portfolio</Link>
-                </NavItem>
-                <NavItem>
-                    <Link to="/contact">Contact</Link>
-                </NavItem>
+            <Navbar>
+                <div>
+                    <style>
+                        {`
+                            #root > div > div {
+                            z-index: 99999 !important;
+                            }
+                        `}
+                    </style>
+                    <SideNav
+                        id="SideNav-10"
+                        options={{
+                        draggable: true
+                        }}
+                        trigger={<Button node="button" className="sidenavBtn"><Icon>dehaze</Icon></Button>}
+                    >
+                        <SideNavItem
+                        user={{
+                            background: 'https://placeimg.com/640/480/tech',
+                            email: 'tjessee7624@gmail.com',
+                            image: 'static/media/react-materialize-logo.824c6ea3.svg',
+                            name: 'Jon Jessee'
+                        }}
+                        userView
+                        />
+                        <SideNavItem divider/>
+                        <SideNavItem
+                            href="/"
+                            waves
+                            icon={<Icon>home</Icon>}
+                        >
+                            Home
+                        </SideNavItem>
+                        <SideNavItem divider/>
+                        <SideNavItem 
+                            href="/portfolio"
+                            waves
+                            icon={<Icon>art_track</Icon>}
+                            >
+                            Portfolio
+                        </SideNavItem>
+                        <SideNavItem divider />
+                        <SideNavItem
+                            href="/contact"
+                            waves
+                            icon={<Icon>markunread</Icon>}
+                        >
+                            Contact
+                        </SideNavItem>
+                    </SideNav>
+                </div>
             </Navbar>
+            
+            // <Navbar
+            //     className="nav"
+            //     alignLinks="right"
+            //     brand={<Link to="/"><img src={logo} width="180" height="60" alt="'Jessee' logo" className="brand-logo"></img></Link>}
+            //     id="mobile-nav"
+            //     menuIcon={<Icon>menu</Icon>}
+            //     options={{
+            //         draggable: true,
+            //         edge: 'left',
+            //         inDuration: 250,
+            //         onCloseEnd: null,
+            //         onCloseStart: null,
+            //         onOpenEnd: null,
+            //         onOpenStart: null,
+            //         outDuration: 200,
+            //         preventScrolling: true
+            //     }}
+            //     >
+            //     <NavItem>
+            //         <Link to="/">Home</Link>
+            //     </NavItem>
+            //     <NavItem>
+            //         <Link to="/portfolio">Portfolio</Link>
+            //     </NavItem>
+            //     <NavItem>
+            //         <Link to="/contact">Contact</Link>
+            //     </NavItem>
+            // </Navbar>
         );
     };
 };
